2e2148bc4d60b8d5f720b106d412eb8fb608ec47
Anthony Smith <Anthony.JWSmith@gmail.com>
2020-03-10 23:14:16 -0500

    added function to handle right answer clicks

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//variables
+console.log('banana')
 var startBtn = document.getElementById('startButton');
 var qOne = document.getElementById('qOne');
 var qTwo = document.getElementById('qTwo');
@@ -7,3 +9,19 @@ var qFive = document.getElementById('qFive');
 
 var right = document.getElementsByClassName('right');
 var wrong = document.getElementsByClassName('wrong');
+var btn = document.getElementsByClassName('btn');
+var score = 0;
+
+//starts with all questions hidden
+//qOne.style.display = "block";
+
+//if a right answer is clicked then add one to the local file otherwise it does nothing
+if {right.addEventListener('click', function() {
+    score++;
+ localStorage.setItem('score', score);
+});
+} else {
+    return
+}
+
+
