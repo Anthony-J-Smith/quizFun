672dde61c120d2d94f2c42dd9daa7700cfb83c53
Anthony Smith <Anthony.JWSmith@gmail.com>
2020-03-10 22:50:55 -0500

    added right and wrong classes to answer butons

---
+++ script.js
@@ -0,0 +1,5 @@
+var qOne = document.getElementById('qOne');
+var qTwo = document.getElementById('qTwo');
+var qThree = document.getElementById('qThree');
+var qFour = document.getElementById('qFour');
+var qFive = document.getElementById('qFive');
