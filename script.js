a4964b90d5e5a26ea8c8a7eb0b282f0d72128f55
Anthony Smith <Anthony.JWSmith@gmail.com>
2020-03-10 22:52:34 -0500

    linked javascript

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+var startBtn = document.getElementById('startButton');
 var qOne = document.getElementById('qOne');
 var qTwo = document.getElementById('qTwo');
 var qThree = document.getElementById('qThree');
 var qFour = document.getElementById('qFour');
 var qFive = document.getElementById('qFive');
+
+var one = document.getElementsByClassName
